e49a442d4e6fc434c32461a5ebeabc4e07521ed2
Samuel Palmer <palmer.samuel@elev.ga.ntig.se>
2022-11-16 18:37:45 -0900

    Small Work

--- APIAdvanced/Program.cs
+++ APIAdvanced/Program.cs
@@ -3,13 +3,26 @@ using RestSharp;
 using System.IO;
 
 Setup();
+Draw();
 
 void Setup()
 {
     Raylib.InitWindow(938, 938, "Nasa Explorer! - Light");
 }
 
+
 void Draw()
 {
+    string apiKey = "BxUpg7OIRBnsjGirr3IRuUc2v0aeM4IpliYannDv"; //private mail api key, maybe shouldn't be shared on GitHub :D
+    string baseURL = "https://api.nasa.gov/";
 
+    while (!Raylib.WindowShouldClose())
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.WHITE);
+
+
+
+        Raylib.EndDrawing();
+    }
 }
