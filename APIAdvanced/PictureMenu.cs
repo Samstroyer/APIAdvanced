b510d42e6af8f3b97673e3d75deed6faf7f427ff
Samuel Palmer <palmer.samuel@elev.ga.ntig.se>
2022-11-23 22:49:32 -0900

    Create Numpad Ingame

--- APIAdvanced/PictureMenu.cs
+++ APIAdvanced/PictureMenu.cs
@@ -1,4 +1,4 @@
-using System;
+using Raylib_cs;
 
 /* 
 This class file is made for the choosing of picture
@@ -7,9 +7,52 @@ This class is planned to be extremely complex and helpful for the user over anyt
 
 public class PictureMenu
 {
-
-    public void Display()
+    private enum MenuStates
     {
+        EnterSol,
+        ChooseCamera,
+        EnterPictureID,
+        DisplayPicture
+    }
 
+    MenuStates currMenu = MenuStates.EnterSol;
+
+    ChoosenRover choosenRoverContainer;
+
+    public PictureMenu(ChoosenRover cr)
+    {
+        choosenRoverContainer = cr;
+    }
+
+    public void Start()
+    {
+        switch (currMenu)
+        {
+            case MenuStates.EnterSol:
+
+                break;
+            case MenuStates.ChooseCamera:
+
+                break;
+            case MenuStates.EnterPictureID:
+
+                break;
+            case MenuStates.DisplayPicture:
+
+                break;
+        }
+    }
+
+    private void EnterSol()
+    {
+        while (true)
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.WHITE);
+
+
+
+            Raylib.EndDrawing();
+        }
     }
 }
